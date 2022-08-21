3ba534ccd34949703a109c0962eb3f825c43a8a5
gustavokubiack <guukubiack@gmail.com>
2022-08-20 23:37:25 -0300

    shellScript

---
+++ firstScript.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/zsh
+
+echo 'hello world'
+
+echo "executing ls command"
+ls
+
+echo "executing pwd command"
+pwd
+
+echo "executing cd /home"
+cd /home
+
+echo "executing ls again"
+ls
+
+
---
+++ variables.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/zsh
+
+ball="this ball is red"
+echo "$ball"
+
+# this is a comment 
+
+myName="Gustavo Kubiack"
+echo "My name is ${myName}"
+
