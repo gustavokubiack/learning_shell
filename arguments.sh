b45f7d65c8c404752b9d48560591007f570ffed3
gustavokubiack <guukubiack@gmail.com>
2022-08-21 20:45:08 -0300

    passing arguments into terminal

---
+++ arguments.sh
@@ -0,0 +1,9 @@
+#!usr/bin/zsh
+
+# arguments can be passed to a script
+
+echo "File name is "$0 
+
+echo "My name is "$1 # i will write the first argument in terminal
+
+echo "My girlfriend is called "$2 
--- variables.sh
+++ variables.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/zsh
 
+# this is a variable
 ball="this ball is red"
 echo "$ball"
 
 # this is a comment 
-
 myName="Gustavo Kubiack"
 echo "My name is ${myName}"
 
 # storing commnad in a variable 
-
 myHome="/home"
 cd "$myHome"
-
 ls
 
+# special character 
+price_of_apple=2.99
+echo "the price of an apple is: \nR\$ $price_of_apple"
